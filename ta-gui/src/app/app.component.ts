df0d86cc0e92ec4ecc1197d21612401bd96355c8
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:16:32 -0300

    visualizacao da lista de alunos cadastrados

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -14,9 +14,12 @@ export class AppComponent {
 
    aluno: Aluno = {nome: "", cpf: "", email: "",github: ""};
    alunoService = new AlunoService();
+   alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
      this.alunoService.gravar(a);
+     this.alunos.push(a);    
      this.aluno = {nome: "", cpf: "", email: "", github: ""};
+     
   }
 }
