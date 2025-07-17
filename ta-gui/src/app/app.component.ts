111e532c31fb75f0409139abccf42eba80553e10
Igor casa <igorvasconcelos1d@gmail.com>
2025-07-17 14:59:55 -0300

    q27 ok

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
   standalone: true,
   imports: [FormsModule, CommonModule]           // importa ngModel
 })
