130c12900e20b75ce2893d0301d645552110882c
Igor casa <igorvasconcelos1d@gmail.com>
2025-07-17 15:20:24 -0300

    questao 29 ok

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -3,12 +3,14 @@ import { Aluno } from './aluno';
 export class AlunoService {
   alunos: Aluno[] = [];
   
-  gravar(aluno: Aluno): Aluno {
-    var result = null;
-    if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
-      this.alunos.push(aluno);
-      result = aluno;
-    }
-    return result;
+ 
+  gravar(aluno: Aluno): boolean {
+  if (!this.alunos.find(a => a.cpf === aluno.cpf)) {
+    this.alunos.push(aluno);
+    return true;
   }
+  return false;
 }
+
+  }
+
