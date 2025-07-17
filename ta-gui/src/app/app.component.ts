628b99d2beecf4db190ab91d8f1ba14747b4983c
Igor casa <igorvasconcelos1d@gmail.com>
2025-07-17 14:34:54 -0300

    Alterações locais antes do cherry-pick

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -12,11 +12,11 @@ import { AlunoService } from './aluno.service';
 })
 export class AppComponent {
 
-   aluno: Aluno = {nome: "", cpf: "", email: ""};
+   aluno: Aluno = {nome: "", cpf: "", email: "",github: ""};
    alunoService = new AlunoService();
 
    gravar(a: Aluno): void {
      this.alunoService.gravar(a);
-     this.aluno = {nome: "", cpf: "", email: ""};
+     this.aluno = {nome: "", cpf: "", email: "", github: ""};
   }
 }
